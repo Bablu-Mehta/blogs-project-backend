66207094117090b9c466dfe500a03441241f2e76
Bablu Mehta <babloomehta895012@gmail.com>
2025-04-09 21:22:27 +0530

    added auth middleware

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.header("Authorization");
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res
+      .status(401)
+      .json({ message: "Access denied. No token provided." });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(500).json({ message: "invalid token", error });
+  }
+};
+
+module.exports = authMiddleware;
